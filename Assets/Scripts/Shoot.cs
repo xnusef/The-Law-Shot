e10d32ccb9dde654544eee64ad6a0370f1da0e00
Inti Fernandez <intifernandez2004@gmail.com>
2021-05-27 16:40:35 -0300

    Disparo

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -16,74 +16,46 @@ public class Shoot : MonoBehaviour
     public Sprite b4;
     public Sprite b5;
     public Sprite b6;
-    //public Sprite[] mySprites;
-
-    /*    b0,
-        b1,
-        b2,
-        b3,
-        b4,
-        b5,
-        b6
-    };*/
+    public Sprite[] mySprites;
     bool reloading = false;
-
-
-
 
     // Start is called before the first frame update
     void Start()
     {
-        //mySprites = {b0,b1,b2,b3,b4,b5,b6};
-        bulletCount.GetComponent<Image>().sprite = b6;
+        mySprites = new Sprite[7];
+        mySprites[0] = b0;
+        mySprites[1] = b1;
+        mySprites[2] = b2;
+        mySprites[3] = b3;
+        mySprites[4] = b4;
+        mySprites[5] = b5;
+        mySprites[6] = b6;
+        bulletCount.GetComponent<Image>().sprite = mySprites[bullets];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("r") && reloading == false)
+        if (Input.GetKey("r") && reloading == false && bullets < 6)
         {
             reloading = true;
             ReloadBullets();
         }
         if (Input.GetMouseButtonDown(0) && reloading == false && bullets > 0) 
         {
-            Bullets();
-        } else if (Input.GetMouseButtonDown(0) && reloading == true)
+            Shooted();
+        } /*else if (Input.GetMouseButtonDown(0) && reloading == true)
         {
             reloading = false;
-        }
-    }
-
-    public void Bullets()
-    {
-        bullets = bullets - 1;
-        bulletCount.GetComponent<Image>().sprite = b5;
-        /*if (bullets == 5)
-        {
-            bulletCount.GetComponent<Image>().sprite = b5;
-        }
-        if (bullets == 4)
-        {
-            bulletCount.GetComponent<Image>().sprite = b4;
-        }
-        if (bullets == 3)
-        {
-            bulletCount.GetComponent<Image>().sprite = b3;
-        }
-        if (bullets == 2)
-        {
-            bulletCount.GetComponent<Image>().sprite = b2;
-        }
-        if (bullets == 1)
-        {
-            bulletCount.GetComponent<Image>().sprite = b1;
-        }
-        if (bullets == 0)
-        {
-            bulletCount.GetComponent<Image>().sprite = b0;
         }*/
     }
+
+    public void Shooted()
+    {
+        bullets = bullets - 1;
+        bulletCount.GetComponent<Image>().sprite = mySprites[bullets];
+    }
+
     void ReloadBullets()
     {
         StartCoroutine(Reload());
@@ -94,34 +66,9 @@ public class Shoot : MonoBehaviour
         while (bullets < 6 && reloading == true)
         {
             yield return new WaitForSeconds(0.3f);
-            if (bullets == 0)
-            {
-                bulletCount.GetComponent<Image>().sprite = b1;
-            }
-            if (bullets == 1)
-            {
-                bulletCount.GetComponent<Image>().sprite = b2;
-            }
-            if (bullets == 2)
-            {
-                bulletCount.GetComponent<Image>().sprite = b3;
-            }
-            if (bullets == 3)
-            {
-                bulletCount.GetComponent<Image>().sprite = b4;
-            }
-            if (bullets == 4)
-            {
-                bulletCount.GetComponent<Image>().sprite = b5;
-            }
-            if (bullets == 5)
-            {
-                bulletCount.GetComponent<Image>().sprite = b6;
-            }
             bullets = bullets + 1;
+            bulletCount.GetComponent<Image>().sprite = mySprites[bullets];
         }
-        reloading=false;
+        reloading = false;
     }
-        
 }
-
